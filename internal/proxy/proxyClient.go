2eedeb3aaa67e62f4ed87897774b70929660dfc5
Ifelsik <misha2004546@gmail.com>
2025-04-24 00:36:30 +0300

    fix: bug with request repeater

--- internal/proxy/proxyClient.go
+++ internal/proxy/proxyClient.go
@@ -2,8 +2,11 @@ package proxy
 
 import (
 	"bufio"
+	"crypto/tls"
+	"fmt"
 	"net"
 	"net/http"
+	"net/url"
 
 	"github.com/sirupsen/logrus"
 )
@@ -34,6 +37,15 @@ func (p *ProxyClient) Connect() error {
 
 func (p *ProxyClient) SendHTTP(request *http.Request) (*http.Response, error) {
 	// handle TLS
+	if request.URL.Scheme == "https" {
+		p.log.Debug("Establishing TLS connection")
+		err := p.establishTLS(request.Host + ":443")
+		if err != nil {
+			return nil, err
+		}
+		p.log.Debug("TLS connection established")
+	}
+
 	err := request.Write(p.Conn)
 	if err != nil {
 		return nil, err
@@ -43,10 +55,40 @@ func (p *ProxyClient) SendHTTP(request *http.Request) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return resp, nil
 }
 
 func (p *ProxyClient) Close() {
 	p.Conn.Close()
+}
+
+func (p *ProxyClient) establishTLS(targetHost string) error {
+	connectRequest := &http.Request{
+		Method: http.MethodConnect,
+		URL:    &url.URL{Opaque: targetHost},
+		Host:   targetHost,
+		Header: http.Header{
+			"Host": []string{targetHost},
+		},
+	}
+	connectRequest.Write(p.Conn)
+
+	resp, err := http.ReadResponse(bufio.NewReader(p.Conn), connectRequest)
+	if err != nil {
+		return fmt.Errorf("proxy CONNECT failed: %v", err)
+	}
+	if resp.StatusCode != 200 {
+        return fmt.Errorf("proxy CONNECT failed: %s", resp.Status)
+    }
+
+	tlsConn := tls.Client(p.Conn, &tls.Config{
+		ServerName: targetHost,
+		InsecureSkipVerify: true,
+	})
+	if err := tlsConn.Handshake(); err != nil {
+		return err
+	}
+	p.Conn = tlsConn
+	return nil
 }
